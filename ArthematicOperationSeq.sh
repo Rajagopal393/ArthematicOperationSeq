27897b3c01bec0546dd9d365f97b9463def5cbac
Raj Gopal <harirajagopal393@gmail.com>
2020-04-12 19:30:57 +0530

    Added feature to perform fourth operation

--- ArthematicOperationSeq.sh
+++ ArthematicOperationSeq.sh
@@ -6,6 +6,8 @@ read c
 firstOperation=$(( $a+$b*$c ))
 secondOperation=$(( $a*$b+$c ))
 thirdOperation=$(($c+$a/$b))
+fourthOperation=$(($a*$b+$c))
 echo  result after performing first operation is $firstOperation
 echo  result after performing second operation is $secondOperation
 echo  result after performing third operation is $thirdOperation
+echo  result after performing fourth operation is $fourthOperation
