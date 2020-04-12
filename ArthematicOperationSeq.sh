ff625f8e2a5c69d4425ee0c270348bc454e81be9
Raj Gopal <harirajagopal393@gmail.com>
2020-04-12 19:25:18 +0530

    Added feature to perform third operation

--- ArthematicOperationSeq.sh
+++ ArthematicOperationSeq.sh
@@ -3,7 +3,9 @@ echo Enter three values
 read a
 read b
 read c
-firstOperation=$(( $a+($b*$c) ))
-secondOperation=$(( ($a*$b)+$c ))
+firstOperation=$(( $a+$b*$c ))
+secondOperation=$(( $a*$b+$c ))
+thirdOperation=$(($c+$a/$b))
 echo  result after performing first operation is $firstOperation
-echo  result after performing first operation is $secondOperation
+echo  result after performing second operation is $secondOperation
+echo  result after performing third operation is $thirdOperation
