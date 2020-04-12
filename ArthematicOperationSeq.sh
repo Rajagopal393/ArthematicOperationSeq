3220b50a772fc812f6b6a184777f62cf4a1d6a4d
Raj Gopal <harirajagopal393@gmail.com>
2020-04-12 19:46:25 +0530

    Added feature to store the results in an Array

--- ArthematicOperationSeq.sh
+++ ArthematicOperationSeq.sh
@@ -7,7 +7,13 @@ firstOperation=$(( $a+$b*$c ))
 secondOperation=$(( $a*$b+$c ))
 thirdOperation=$(($c+$a/$b))
 fourthOperation=$(($a*$b+$c))
-echo  result after performing first operation is $firstOperation
-echo  result after performing second operation is $secondOperation
-echo  result after performing third operation is $thirdOperation
-echo  result after performing fourth operation is $fourthOperation
+
+
+declare -A resultArray
+
+resultArray[aa]=$firstOperation
+resultArray[bb]=$secondOperation
+resultArray[cc]=$thirdOperation
+resultArray[dd]=$fourthOperation
+
+echo "result : " ${resultArray[@]}
