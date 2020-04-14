c4718628e831ea68c6e69bee5ae2646e8e369271
Raj Gopal <harirajagopal393@gmail.com>
2020-04-14 14:22:50 +0530

    Added feature to sort the obtained results in Descending order

--- ArthematicOperationSeq.sh
+++ ArthematicOperationSeq.sh
@@ -6,18 +6,31 @@ read c
 firstOperation=$(( $a+$b*$c ))
 secondOperation=$(( $a*$b+$c ))
 thirdOperation=$(($c+$a/$b))
-fourthOperation=$(($a*$b+$c))
-
+fourthOperation=$(($a%$b+$c))
 
 declare -A resultArray
 
-resultArray[aa]=$firstOperation
-resultArray[bb]=$secondOperation
-resultArray[cc]=$thirdOperation
-resultArray[dd]=$fourthOperation
+resultArray[1]=$firstOperation
+resultArray[2]=$secondOperation
+resultArray[3]=$thirdOperation
+resultArray[4]=$fourthOperation
 
 echo "result : " ${resultArray[@]}
 
 echo "results sorted in ascending order :"
 
 printf '%s\n' "${resultArray[@]}" | sort -n
+
+for ((i=0; i<${#resultArray[@]}; i++)) 
+	do
+    	  for ((j=$i; j<${#resultArray[@]}; j++)) 
+   		 do
+     		 temp=${resultArray[$i]}; 
+        	 if [[ $temp -lt ${resultArray[$j]} ]]; then
+            	    resultArray[$i]=${resultArray[$j]}  
+            	    resultArray[$j]=$temp
+        	fi
+                done
+	done
+echo "results sorted in Descending order : "
+echo ${resultArray[@]} 
